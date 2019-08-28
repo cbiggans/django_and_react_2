08286d47c60c4e9e1ac5856a99c746c47535a018
Clarence Biggans <clarencebiggans@gmail.com>
2019-08-28 15:52:13 -0700

    Initial Commit

---
+++ backend/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/clarenceb/programming/react/real_estate_app_2/backend/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
